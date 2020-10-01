261384c09fcb9eac6394dd73ba058d8a79b26ce9
Daniel Villar <dvillarmo@gmail.com>
2020-10-01 12:18:27 -0500

    added technical and scrape

---
+++ Technicals.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  1 12:11:27 2020
+
+@author: Daniel
+"""
+
+def RSI(daily_info_60, ticker):
+    
+    
+    rsi_array = len(daily_info_60[daily_info_60['Ticker']==ticker])
+    
+    for i in range(rsi_array):
+        RS = #avg N day close up / avg N day close down
+        RSI = 100 - (100/1 + RS)
+
+def Chaikin(daily_info_60, ticker):
+    
+    
+def EMA50(daily_info_60,ticker):
+    
+    
+def Current_Price():
+    
+        
+    
---
+++ ticker_scrape.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  1 12:17:27 2020
+
+@author: Daniel
+"""
+
+class TickerScrape():
+    
