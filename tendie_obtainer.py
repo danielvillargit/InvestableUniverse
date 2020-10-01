0b83ade94eade79591792e65175abfd83a8b884c
Daniel Villar <dvillarmo@gmail.com>
2020-09-30 20:38:26 -0500

    Modified Xpath for Marketcap and Industry

--- tendie_obtainer.py
+++ tendie_obtainer.py
@@ -53,15 +53,18 @@ def GetEarnings(self):
         
         print(csv3)
         count_ = 0
+        
+        #i needs to be csvimport from Exchange in this case
         for j,i in enumerate(ar_):
             url_ = r"https://finviz.com/quote.ashx?t={}&ty=c&ta=1&p=d".format(i)
             driver.get(url_)
-            edate =driver.find_element_by_xpath("/html/body/table[3]/tbody/tr[1]/td/table/tbody/tr[7]/td/table/tbody/tr[11]/td[6]").text
-            print(edate)
-            beta = driver.find_element_by_xpath("/html/body/table[3]/tbody/tr[1]/td/table/tbody/tr[7]/td/table/tbody/tr[7]/td[12]").text
-            csv3.loc[count_,'Earnings Date'] = edate
-            csv3.loc[count_,'Beta'] = beta
-            print(csv3['Earnings Date'])
+            marketcap =driver.find_element_by_xpath("/html/body/table[3]/tbody/tr[1]/td/table/tbody/tr[7]/td/table/tbody/tr[2]/td[2]").text
+            print(marketcap)
+            
+            industry = driver.find_element_by_xpath("/html/body/table[3]/tbody/tr[1]/td/table/tbody/tr[6]/td/table/tbody/tr[2]/td/table/tbody/tr[3]/td/a[1]").text
+            csv3.loc[count_,'Industry'] = industry
+            csv3.loc[count_,'MarketCap'] = marketcap
+            print(csv3['Industry'])
             count_ = count_+1
             if j==5:
                 break
